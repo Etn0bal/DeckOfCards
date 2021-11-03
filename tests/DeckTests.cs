86dee4d550cf5adb3142452910eb8e4f59c2ef12
Antoine Labonté <antoinelabo@videotron.ca>
2021-11-03 12:22:42 -0400

    added test project and solution

---
+++ tests/DeckTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DeckOfCards.Tests
+{
+    public class DeckTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
